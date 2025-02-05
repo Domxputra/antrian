755af5127b2109f6fa34772d71e3411e2b8071f6
Domxputra <rajinberfoto@mail.com>
2025-02-05 19:05:56 +0900

    new

--- app/Views/layout.php
+++ app/Views/layout.php
@@ -65,20 +65,19 @@
                 <div id="collapseUtilities" class="collapse" aria-labelledby="headingUtilities"
                     data-parent="#accordionSidebar">
                     <div class="bg-white py-2 collapse-inner rounded">
-                        <h6 class="collapse-header">Custom Utilities:</h6>
-                        <a class="collapse-item" href="utilities-color.html">Colors</a>
-                        <a class="collapse-item" href="utilities-border.html">Borders</a>
-                        <a class="collapse-item" href="utilities-animation.html">Animations</a>
-                        <a class="collapse-item" href="utilities-other.html">Other</a>
+                        <h6 class="collapse-header">Daftar Antrian</h6>
+                        <a class="collapse-item" href="utilities-color.html"><i class="fas fa-ticket-alt"></i>Ambil Antrian</a>
+                        <a class="collapse-item" href="utilities-border.html"><i class="fas fa-volume-up"></i>Panggil Antrian</a>
+                        <a class="collapse-item" href="utilities-animation.html"><i class="fas fa-undo"></i>Reset antrian</a>
+                        <a class="collapse-item" href="utilities-other.html"><i class="fas fa-chart-bar"></i>Rekap Antrian</a>
                     </div>
                 </div>
             </li>
-
             <!-- Nav Item - Utilities Collapse Menu -->
             <li class="nav-item">
                 <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseUtilities"
                     aria-expanded="true" aria-controls="collapseUtilities">
-                    <i class="fas fa-ticket-alt"></i>
+                    <i class="fas fa-desktop"></i>
                     <span>Ambil Antrians</span>
                 </a>
                 <div id="collapseUtilities" class="collapse" aria-labelledby="headingUtilities"
